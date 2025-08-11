6692c27ad79bb7c4f033556f1d58e65e5d93e881
ahmadreza25 <ywsfwnda8@gmail.com>
2025-08-11 18:59:18 +0330

    ezafeh kardan list dokmeh cpehei mono

---
+++ src/Pages/Menu/ListBtnMenu.tsx
@@ -0,0 +1,41 @@
+const ListBtnMenu = () => {
+  return (
+    <div>
+        <div className="w-[450px] flex flex-col mt-[50px]">
+            <div className="ml-[80px] mt-[30px]">
+                <h1 className="font-[700] text-[18px]">Drinks</h1>
+                <ul className="mt-[20px]">
+                    <li className="font-[400] text-[18px] text-slate-500 mt-[15px]">Hot Coffee</li>
+                    <li className="font-[400] text-[18px] text-slate-500 mt-[15px]">Cold Coffee</li>
+                    <li className="font-[400] text-[18px] text-slate-500 mt-[15px]">Hot Tea</li>
+                    <li className="font-[400] text-[18px] text-slate-500 mt-[15px]">Cold Tea</li>
+                    <li className="font-[400] text-[18px] text-slate-500 mt-[15px]">Refreshers</li>
+                    <li className="font-[400] text-[18px] text-slate-500 mt-[15px]">Frappuccino® Blended Beverage</li>
+                    <li className="font-[400] text-[18px] text-slate-500 mt-[15px]">Hot Chocolate, Lemonade & More</li>
+                    <li className="font-[400] text-[18px] text-slate-500 mt-[15px]">Bottled Beverages</li>
+                </ul>
+            </div>
+            <div className="ml-[80px] mt-[30px]">
+                <h1 className="font-[700] text-[18px]">Food</h1>
+                <ul className="mt-[20px]">
+                    <li className="font-[400] text-[18px] text-slate-500 mt-[15px]">Breakfast</li>
+                    <li className="font-[400] text-[18px] text-slate-500 mt-[15px]">Bakery</li>
+                    <li className="font-[400] text-[18px] text-slate-500 mt-[15px]">Treats</li>
+                    <li className="font-[400] text-[18px] text-slate-500 mt-[15px]">Lunch</li>
+                    <li className="font-[400] text-[18px] text-slate-500 mt-[15px]">Snacks</li>
+                </ul>
+            </div>
+            <div className="ml-[80px] mt-[30px]">
+                <h1 className="font-[700] text-[18px]">At Home Coffee</h1>
+                <ul className="mt-[20px]">
+                    <li className="font-[400] text-[18px] text-slate-500 mt-[15px]">Whole Bean</li>
+                    <li className="font-[400] text-[18px] text-slate-500 mt-[15px]">VIA® Instant</li>
+                    <li className="font-[400] text-[18px] text-slate-500 mt-[15px]">Shopping Bag</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ListBtnMenu
--- src/Pages/Menu/Menu.tsx
+++ src/Pages/Menu/Menu.tsx
@@ -1,9 +1,14 @@
 import HeadMenu from "./HeadMenu"
+import ListBtnMenu from "./ListBtnMenu"
 const Menu = () => {
   return (
     <div>
         <div>
             <div><HeadMenu /></div>
+            <div className="flex items-center">
+                <div><ListBtnMenu /></div>
+                <div></div>
+            </div>
         </div>
     </div>
   )
